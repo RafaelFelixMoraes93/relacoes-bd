ba4cc870f7d7f257fd5970f7b7512bc3c9480cd5
Rafael <rafael.felix.moraes@gmail.com>
2025-05-03 19:54:59 -0300

    Feat view Relacoes

---
+++ resources/views/relacoes.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Relacionamentos</title>
+</head>
+<body>
+    <h1>Relacionamentos</h1>
+    <p>Relacionamentos é a forma como os dados se conectam, então podemos ter N tabelas se conectando com N tabelas.</p>
+
+    <h2>Has One</h2>
+    <p>'Has One' ou 'Tem Um' tem uma relação de 1:1, aqui é apenas 1 dado para 1 dado.</p>
+
+    <h2>Belongs To</h2>
+    <p>'Belongs To' ou 'Pertence a', tem uma relaçao de N:1, indica 1 relação para muitos.</p>
+
+    <h2>Has Many</h2>
+    <p>'Has Many' ou 'Tem Muitos', tem uma relaçao de 1:N, indica muitos para 1.</p>
+
+    <h2>Many to Many</h2>
+    <p>'Many to Many' ou 'Muitos para Muitos', tem uma relação de N:N.</p>
+    
+</body>
+</html>
